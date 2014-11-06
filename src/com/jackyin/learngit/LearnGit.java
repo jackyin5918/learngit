bb709c67bb88bedf611c9a2476d0ddd746f65827
Jack Yin <277940225@qq.com>
2014-11-06 14:11:16 +0800

    mushroomA

--- src/com/jackyin/learngit/LearnGit.java
+++ src/com/jackyin/learngit/LearnGit.java
@@ -1,15 +1,15 @@
 package com.jackyin.learngit;
 
 public class LearnGit {
-	private int mushroom = 0;
+	private int mushroomA = 0;
 	
 	public LearnGit(int mushroom) {
-		this.mushroom = mushroom;
+		this.mushroomA = mushroom;
 	}
 	
 	public static void main(String[] args) {
 	  System.out.println("I'm learning Git!");
 	  LearnGit learnGit = new LearnGit(3);
-	  System.out.println("mushroom = " + learnGit.mushroom);
+	  System.out.println("mushroom = " + learnGit.mushroomA);
   }
 }
